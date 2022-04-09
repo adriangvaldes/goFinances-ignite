334c8e65fc90c0e09378b302d3d34eff83899f9b
Adrian Valdes <adriangvaldes@gmail.com>
2022-04-09 04:14:24 -0300

    init

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>GO Finances</Text>
       <StatusBar style="auto" />
     </View>
   );
